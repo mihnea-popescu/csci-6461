9e273f9ff966ee66ac428f73fad138f6f7001e54
Mihnea Popescu <mihnea.popescu56@gmail.com>
2025-10-16 15:37:53 -0400

    change directory structure

---
+++ com/project/instruction/Instruction.java
@@ -0,0 +1,4 @@
+package com.project.instruction;
+
+public class Instruction {
+}
---
+++ com/project/util/Constants.java
@@ -0,0 +1,12 @@
+package com.project.util;
+
+/**
+ * Here we will save the hardcoded constants
+ * given by the project description
+ */
+public class Constants {
+    public short WORD_SIZE = 16;
+    public short MEMORY_SIZE = 2048;
+    public short NUM_GPRS = 4;
+    public short NUM_IXRS = 3;
+}
