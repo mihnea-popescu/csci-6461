ba9ea9ed75bb1069ee78cb767dab7517600e34a4
x jx <sxu79@gwu.edu>
2025-10-31 23:55:56 -0400

    update

--- com/project/loader/RomLoader.java
+++ com/project/loader/RomLoader.java
@@ -8,17 +8,27 @@ import java.util.List;
 
 public class RomLoader {
     public static void loadInstructionsInMemory(Cpu cpu, List<Integer> binaryCodes) {
-        int ins1 = binaryCodes.get(0);
-        int a = 0;
+        
         int address = 0;
-        if (ins1 >> 10 == 63) { // LOC instruction
-            a = 1;
-            address = ins1 & 0b1111111111;
-            cpu.PC.setValue(address);
-        }
-        for( int i = a; i < binaryCodes.size();i++) {
+        for( int i = 0; i < binaryCodes.size();i++) {
             try {
-                cpu.mem.write(address++, (short) (int)binaryCodes.get(i));
+                int ins1 = binaryCodes.get(i);
+                
+                if (ins1 >> 10 == 63) { // LOC instruction
+                    address = ins1 & 0b1111111111;
+                    cpu.PC.setValue(address);
+                }
+                else if (ins1 >> 10 == 62) { // Data instruction
+                    int data = ins1 & 0b1111111111;
+                    cpu.mem.write(address, (short)(data));
+                    address++;
+                    cpu.PC.setValue(address);
+                }
+                else{
+                    cpu.mem.write(address, (short) (int)binaryCodes.get(i));
+                    address++;
+                }
+                
             } catch (MemoryAccessException e) {
                 System.out.println("MEMORY ACCESS EXCEPTION!"); //Exception
             }
